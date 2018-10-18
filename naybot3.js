c21b784745fe14930f325911f1be226a7cb6f8e4
naybotnay <43154410+naybotnay@users.noreply.github.com>
2018-10-18 12:39:16 +0300

    Update naybot3.js

--- naybot3.js
+++ naybot3.js
@@ -39,7 +39,7 @@ client.on("message", function (message){
     }
 
 if(mess.startsWith(prefix + "play")) {
-    if(member.voiceChannel || client.guilds.get("488334414124810240").voiceConnection != null){
+    if(member.voiceChannel || client.guilds.get("487982547729383424").voiceConnection != null){
         if(guilds[message.guild.id].queue.length > 0 || guilds[message.guild.id].isPlaying) {
         getID(args, function (id){
             add_to_queue(id, message);
