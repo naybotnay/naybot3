f4967156b9f0a8f5aaa89d5c9f5ea0cbb991478e
naybotnay <43154410+naybotnay@users.noreply.github.com>
2018-10-18 10:59:15 +0300

    Update naybot3.js

--- naybot3.js
+++ naybot3.js
@@ -172,9 +172,9 @@ function getID(str, cb) {
 
 function add_to_queue(strID, message) {
     if (isYoutube(strID)) {
-        guilds[message.guild.id].queue.push(getYouTubeID(strID));
+        guild[message.guild.id].queue.push(getYouTubeID(strID));
     } else {
-        guilds[message.guild.id].queue.push(strID);
+        guild[message.guild.id].queue.push(strID);
     }
 }
 
